43b52b1d5dc2a2cd18c6eb6a3386f4effd6dbe3a
WreckItBoss <isseymatsumoto0911@gmail.com>
2024-08-07 17:54:40 +0900

    replaced

--- betatest/src/pages/top/TopPage.tsx
+++ betatest/src/pages/top/TopPage.tsx
@@ -1,19 +1,59 @@
 import React from 'react';
+
 import { useState, useMemo } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import Map, {Marker, Popup, NavigationControl} from 'react-map-gl';
+import Map, { Marker, Popup, NavigationControl } from 'react-map-gl';
 import styles from './TopPage.module.scss';
+import redframe from '@/assets/red.png';
 
 // for testing
 const TESTDATA = [
-  {"title":"東京タワー", "latitude":35.658621,"longitude":139.745435,"image":"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/TaroTokyo20110213-TokyoTower-01min.jpg/200px-TaroTokyo20110213-TokyoTower-01min.jpg"},
-  {"title":"皇居","latitude":35.68530135769214,"longitude":139.75287460184867,"image":"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Kokyo0057.jpg/1920px-Kokyo0057.jpg"},
+  {
+    title: '東京タワー',
+    latitude: 35.658621,
+    longitude: 139.745435,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/TaroTokyo20110213-TokyoTower-01min.jpg/200px-TaroTokyo20110213-TokyoTower-01min.jpg',
+  },
+  {
+    title: '皇居',
+    latitude: 35.68530135769214,
+    longitude: 139.75287460184867,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Kokyo0057.jpg/1920px-Kokyo0057.jpg',
+  },
 ];
 
 const TopPage: React.FC = () => {
   const navigate = useNavigate();
-  const [popupInfo, setPopupInfo] = useState<{ title: string; latitude: number; longitude: number; image: string } | null>(null);
+  const renderStars = (rating: number) => {
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 !== 0 ? 1 : 0;
+    const emptyStars = 5 - fullStars - halfStar;
+
+    return (
+      <div className={styles.stars}>
+        {[...Array(fullStars)].map((_, i) => (
+          <span key={i} className={styles.fullStar}>
+            ★
+          </span>
+        ))}
+        {halfStar === 1 && <span className={styles.halfStar}>☆</span>}
+        {[...Array(emptyStars)].map((_, i) => (
+          <span key={i} className={styles.emptyStar}>
+            ☆
+          </span>
+        ))}
+      </div>
+    );
+  };
+  const [popupInfo, setPopupInfo] = useState<{
+    title: string;
+    latitude: number;
+    longitude: number;
+    image: string;
+  } | null>(null);
 
   const pins = useMemo(
     () =>
@@ -29,15 +69,12 @@ const TopPage: React.FC = () => {
           }}
         />
       )),
-    []
+    [],
   );
 
   return (
     <div className={styles.container}>
-      <button onClick={() => navigate('/login')}>Log in</button>
-      <Link to='/login'>
-        <p>管理者はこちら</p>
-      </Link>
+      <div className={styles.title}>近くにある避難所</div>
       <div style={{ position: 'relative', height: '500px', width: '100%' }}>
         <Map
           initialViewState={{
@@ -46,7 +83,7 @@ const TopPage: React.FC = () => {
             zoom: 12,
           }}
           style={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }}
-          mapStyle="mapbox://styles/mapbox/streets-v12"
+          mapStyle='mapbox://styles/mapbox/streets-v12'
           mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
         >
           {pins}
@@ -58,19 +95,40 @@ const TopPage: React.FC = () => {
               onClose={() => setPopupInfo(null)}
             >
               <div>
-              {popupInfo.title} |{' '}
-              <a
-                target="_new"
-                href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.title}`}
-              >
-                Wikipedia
-              </a>
-            </div>
-            <img width="100%" src={popupInfo.image} />
+                {popupInfo.title} |{' '}
+                <a
+                  target='_new'
+                  href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.title}`}
+                >
+                  Wikipedia
+                </a>
+              </div>
+              <img width='100%' src={popupInfo.image} />
             </Popup>
           )}
           <NavigationControl />
         </Map>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.contentWrapper}>
+          <div className={styles.subtitle}>避難所情報</div>
+          <div className={styles.redFrameContainer}>
+            <img
+              src={redframe}
+              alt='Redframe'
+              className={styles.redFrameImage}
+            />
+            <div className={styles.frameContent}>
+              <div>避難所の名前</div>
+              <div className={styles.starsContainer}>
+                <div>避難レベル</div>
+                {renderStars(3.5)}
+                <div>3.5</div>
+              </div>
+              <div className={styles.address}>住所：。。。</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
